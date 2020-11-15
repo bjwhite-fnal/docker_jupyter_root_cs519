a8719253424b8c4bf985103d7b01c8051d5f296b
Brandon White <bjwhite@fnal.gov>
2020-11-15 15:22:54 -0600

    Make start script better.

--- start_docker_root.sh
+++ start_docker_root.sh
@@ -1,1 +1,4 @@
-docker run -p 127.0.0.1:8888:8888 -v ${PWD}/notebook:/home/jovyan/work hfukuda/root-jupyter-notebook
+docker run \
+    -p 127.0.0.1:8888:8888 \
+    -v ${PWD}/notebook:/home/jovyan/work \
+    hfukuda/root-jupyter-notebook
